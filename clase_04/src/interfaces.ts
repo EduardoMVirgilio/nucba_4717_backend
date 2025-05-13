73759ea4eadcc62d69fc363423af1e7d0d8621d3
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-05-13 13:26:35 -0300

    Start | Clase 04

--- clase_04/src/interfaces.ts
+++
@@ -1,7 +0,0 @@
-interface IUsuario {
-  nombre: string;
-  edad?: number;
-  rol: "admin" | "user" | "guest"; // unions o uniones
-}
-
-let usuario: IUsuario = { nombre: "edu", rol: "guest" };
--- clase_04/src/main.ts
+++ clase_04/src/main.ts
@@ -1,11 +1,9 @@
-var nombre: string = "edu";
-var edad: number = 29;
-var esMayor: boolean = true;
-var mensajes: string[] = ["Hola", "Llego", "Se fue"];
-var secuencia: number[] = [1, 2, 3];
-let cualquiera: any = [1, 2, 3, "hola"];
-cualquiera = 12;
-console.log(typeof mensajes);
+// Arrow functions
 
-let a: unknown = 1;
-console.log(a as string);
+// Use this
+
+// Herencia en Tipos
+
+// Herencia en Interfaces
+
+// Herencia en Clases
