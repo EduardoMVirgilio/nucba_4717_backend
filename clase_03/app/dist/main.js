cbde6de2700a703196acde399bacc1befbc4646f
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-05-13 13:19:53 -0300

    Start | Clase 04

--- clase_03/app/dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let nombre = "edu";
-nombre = 19;
-console.log(nombre);
-//# sourceMappingURL=main.js.map
---
+++ clase_04/src/main.ts
@@ -0,0 +1,11 @@
+var nombre: string = "edu";
+var edad: number = 29;
+var esMayor: boolean = true;
+var mensajes: string[] = ["Hola", "Llego", "Se fue"];
+var secuencia: number[] = [1, 2, 3];
+let cualquiera: any = [1, 2, 3, "hola"];
+cualquiera = 12;
+console.log(typeof mensajes);
+
+let a: unknown = 1;
+console.log(a as string);
